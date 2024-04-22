4381fad605293e9837d9d0e947c8aa38c1a88d72
Oluwajuwon <oiayanniran@student.lautech.edu.ng>
2024-04-22 20:32:42 +0100

    fixed server controller

--- src/Controllers/InstallServerController.php
+++ src/Controllers/InstallServerController.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace dacoto\LaravelWizardInstaller\Controllers;
 
 use Illuminate\Contracts\Foundation\Application;
@@ -14,14 +12,14 @@ class InstallServerController extends Controller
     public function __invoke(): Factory|View|Application
     {
         return view('installer::steps.server', [
-            'result' => $this->check(),
+            'result' => $this->check()
         ]);
     }
 
     public function check(): bool
     {
         foreach (config('installer.server') as $check) {
-            if (($check['check']['type'] === 'php' && PHP_VERSION_ID < $check['check']['value']) || ($check['check']['type'] === 'extension' && ! extension_loaded($check['check']['value']))) {
+            if (!$check['check']()) {
                 return false;
             }
         }
